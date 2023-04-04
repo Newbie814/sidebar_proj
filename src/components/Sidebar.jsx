faeef126927ef3609e9d0b2134764355b02e21bd
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-04 17:05:59 -0400

    setup sidebar skeleton and styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,45 @@
+import { FaTimes } from 'react-icons/fa';
+import { social, links } from '../data';
+import { useGlobalContext } from '../context';
+
+const logoUrl =
+  'https://res.cloudinary.com/dylvkdabj/image/upload/v1680496923/website%20pics%20family/development_logo_hpyyun.jpg';
+
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
   return (
-    <div>Sidebar</div>
-  )
-}
-export default Sidebar
+    <aside className='sidebar'>
+      <div className='sidebar-header'>
+        <img src={logoUrl} alt='logo' style={{ height: '80px' }} />
+        <button className='close-btn' onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className='links'>
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      {/* id, social, icon */}
+      <ul className='social-links'>
+        {social.map((socialLink) => {
+          const { id, url, icon } = socialLink;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
+export default Sidebar;
